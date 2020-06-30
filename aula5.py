7461090110a4676413b1f8f6320b8ef0b647584c
Your Name <you@example.com>
2020-06-30 17:19:01 -0300

    Adicao da Aula 5

---
+++ aula5.py
@@ -0,0 +1,74 @@
+lista = [12,10,7,5]
+lista_animal = ['cachorro', 'gato', 'elefante', 'lobo', 'arara']
+# print(lista_animal[1])
+# print(min(lista_animal))
+# print(max(lista_animal))
+
+lista_animal[0] = 'macaco'
+print(lista_animal)
+
+tupla = (1, 10, 12, 14)
+print(tupla[3])
+print(len(lista_animal))
+
+tupla_animal = tuple(lista_animal)
+print(type(tupla_animal))
+print(tupla_animal)
+
+lista_numerica = list(tupla)
+print(type(lista_numerica))
+print(lista_numerica)
+
+
+
+# lista.sort()
+# lista_animal.sort()
+# print(lista)
+# print(lista_animal)
+#
+# lista_animal.reverse()
+# print(lista_animal)
+
+
+# if 'lobo' in lista_animal:
+#     print('existe lobo na lista')
+# else:
+#     print('nao existe lobo na lista. Sera incluido.')
+#     lista_animal.append('lobo')
+#     print(lista_animal)
+
+
+# lista_animal.remove('elefante')
+# print(lista_animal)
+
+
+# lista_animal.pop(0)
+# lista_animal.pop(0)
+# print(lista_animal)
+
+
+
+# nova_lista = lista_animal * 3
+# print(nova_lista)
+
+# if 'gato' in lista_animal:
+#     print('existe gato na lista')
+# else:
+#     print('nao existe gato na lista')
+
+
+
+# if 'gato' in lista_animal:
+#     print('existe gato na lista')
+# else:
+#     print('nao existe gato na lista')
+
+
+
+# print(min(lista))
+# print(max(lista))
+# print(sum(lista))
+# soma = 0
+# for x in lista:
+#     soma += x
+# print(soma)
